7414b041b20da45dedee33561ee2981e07be76e9
David Brooks <davidbrooks.now@gmail.com>
2014-01-28 17:29:06 -0500

    Adding feature to delete pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,12 @@ class PicturesController < ApplicationController
         @picture = Picture.new
   end
 
+   def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
   def create
   @picture = Picture.new(picture_params)
     if @picture.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Photogurr::Application.routes.draw do
-  root :to => "pictures#index"
   get 'pictures' => 'pictures#index'
   post 'pictures' => "pictures#create"
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
   get 'pictures/new' => 'pictures#new' 
   get 'pictures/:id' => 'pictures#show', as: "picture"
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  root :to => "pictures#index"
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
